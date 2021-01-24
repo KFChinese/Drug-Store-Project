9fbcad3405561cbb86ca4cc56bb61cc53e6ae664
KFChinese <larrychiem@gmail.com>
2021-01-24 11:46:51 -0800

    Added Part 2

--- project1.sql
+++ project1.sql
@@ -5,3 +5,62 @@ CREATE DATABASE Drugs_R_Us;
 --Selecting Drugs R Us
 USE Drugs_R_Us;
 
+
+-- Creating table drugs
+CREATE TABLE drugs
+(     drugid            CHAR(3)     NOT NULL,
+    drugtradename     VARCHAR(25)     NOT NULL,
+    drugformula         VARCHAR(25)     NOT NULL,
+    pharamacompid    CHAR(3)        NOT NULL,
+    PRIMARY KEY (drugid),
+    FOREIGN KEY (pharmacompid)  REFERENCES pharmacycompany(pharmacompid));
+    
+-- Creating table stock
+CREATE TABLE stock
+(
+    stockdatakey            CHAR(3)     NOT NULL,
+    drugid            CHAR(3)     NOT NULL,
+    pharmadrugprice NUMERIC(7,2)     NOT NULL,
+    pharmacyid CHAR(3) NOT NULL, 
+    PRIMARY KEY (stockdatakey),
+    FOREIGN KEY (drugid)  REFERENCES drugs(drugid),
+    FOREIGN KEY (pharmacyid)  REFERENCES pharmacy(pharmacyid)
+);
+
+-- Creating table perscription
+CREATE TABLE perscription
+(
+    perscriptiondatakey            CHAR(3)     NOT NULL,
+    patientssn            CHAR(9)     NOT NULL,
+    doctorssn            CHAR(9)     NOT NULL,
+    drugid                 CHAR(3) NOT NULL, 
+    PRIMARY KEY (perscriptiondatakey),
+    FOREIGN KEY (drugid)  REFERENCES drugs(drugid),
+    FOREIGN KEY (patientssn)  REFERENCES patient(patientssn),
+    FOREIGN KEY (doctorssn) REFERENCES doctor(doctorssn)
+    
+);
+
+-- Creating table filledperscription
+CREATE TABLE filledperscription
+(
+    perscriptiondatakey            CHAR(3)     NOT NULL,
+    filldate DATE NOT NULL,
+    pharmacyid CHAR(3) NOT NULL, 
+    PRIMARY KEY (perscriptiondatakey),
+    FOREIGN KEY (pharmacyid)  REFERENCES pharmacy(pharmacyid)
+    
+);
+
+-- creating table contracts
+CREATE TABLE contracts
+(
+    contracttext CHAR(5000),
+    supervisorid CHAR(3) NOT NULL,
+    startdate DATE,
+    enddate DATE,
+    pharmacyid CHAR(3) NOT NULL, 
+    pharmacompid CHAR(3) NOT NULL,
+    FOREIGN KEY (pharmacyid)  REFERENCES pharmacy(pharmacyid),
+    FOREIGN KEY (pharmacompid)  REFERENCES pharmacycompany(pharmacompid)
+);
